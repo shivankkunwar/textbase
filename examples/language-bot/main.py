4dbd23578685f1ccd43db49a57629e0ab8f25076
shivank <shivankkunwar100@gmail.com>
2023-09-04 11:23:24 +0530

    translate and progress functionality added

--- examples/language-bot/main.py
+++ examples/language-bot/main.py
@@ -9,6 +9,7 @@ OpenAI.api_key = ""
 SYSTEM_PROMPT = """You are chatting with an AI language learning assistant. You can practice and learn a new language with me. 
 I can provide vocabulary, quizzes, and even engage in conversations in your chosen language. 
 Feel free to ask for vocabulary, grammar tips, or start a conversation to practice.
+Available options: "vocabulary", "quiz"
 """
 
 @bot()
@@ -22,7 +23,7 @@ def on_message(message_history: List[Message], state: dict = None):
         if "vocabulary" in user_message[0]['value'].lower():
             # Provide a vocabulary word and its translation
             bot_response = OpenAI.generate(
-            system_prompt="Generate a new random un related to message history word and giveits translation.",
+            system_prompt="ask for what language user want then generate a new random un related to message history word and giveits translation.",
             message_history=message_history,
             model="gpt-3.5-turbo",
             )
@@ -35,6 +36,22 @@ def on_message(message_history: List[Message], state: dict = None):
             model="gpt-3.5-turbo",
             )
             response_text = bot_response
+        elif "translate" in user_message[0]['value'].lower():
+            # Generate a language quiz question
+            bot_response = OpenAI.generate(
+            system_prompt="Ask for a phrase from user and what language you want the phrase to be translated then generate the translated phrase in that language",
+            message_history=message_history,
+            model="gpt-3.5-turbo",
+            )
+            response_text = bot_response    
+        elif "progress" in user_message[0]['value'].lower():
+            # Generate a language quiz question
+            bot_response = OpenAI.generate(
+            system_prompt="generate a progress report from previously correct answers provided by the user",
+            message_history=message_history,
+            model="gpt-3.5-turbo",
+            )
+            response_text = bot_response  
         else:
             # Generate a natural language response
             bot_response = OpenAI.generate(
